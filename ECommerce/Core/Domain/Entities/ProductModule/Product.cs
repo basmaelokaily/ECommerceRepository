2955379ab77d5519c20e950a61a5dbad451449b2
basma elokaily <basma.abdelhaleem@x-inity.com>
2025-11-21 11:13:41 +0200

    Add project files.

---
+++ ECommerce/Core/Domain/Entities/ProductModule/Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities.ProductModule
+{
+    public class Product : BaseEntity<int>
+    {
+        public string Name { get; set; }
+        public string Description { get; set; } = null;
+        public string PictureUrl { get; set; } = null;
+        public decimal Price { get; set; }
+
+        //one to many product and brand
+        public ProductBrand ProductBrand { get; set; }
+        public int BrandId { get; set; }
+
+        public ProductType ProductType { get; set; }
+        public int TypeId { get; set; }
+
+    }
+}
---
+++ ECommerce/Infrastructure/Presistences/Data/StoreDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Presistences.Data
+{
+    public class StoreDbContext : DbContext
+    {
+        public StoreDbContext(DbContextOptions<StoreDbContext> options) :base(options)
+        {
+
+        }
+    }
+}
